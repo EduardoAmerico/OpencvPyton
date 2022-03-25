ead9072fcf4921130ba9adb21bf17a5b8f28c96c
EduardoAmerico <eduardoamerico_@live.com>
2022-03-25 14:18:12 -0300

    Estudo capitulo 6 e 7

---
+++ charpter6.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("Resources/Lenna.png")
+
+hor = np.hstack((img,img))
+ver = np.vstack((img,img))
+
+cv2.imshow("horizontal", hor)
+cv2.imshow("vertical", ver)
+cv2.waitKey(0)
+
